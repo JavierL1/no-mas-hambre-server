db27f7dfc25b96ee62a4fb7baf515e5f65800509
Javier Levio <javier.levio.12@sansano.usm.cl>
2020-06-13 15:21:29 -0400

    Added atencion necesidad to admin

--- web/app/formularios/admin.py
+++ web/app/formularios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Necesidad
+from .models import Necesidad, AtencionNecesidad, EstadoAtencionNecesidad
 
 # Register your models here.
 
@@ -7,3 +7,8 @@ from .models import Necesidad
 @admin.register(Necesidad)
 class NecesidadAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(AtencionNecesidad)
+class AtencionNecesidadAdmin(admin.ModelAdmin):
+    list_display = ['necesidad', 'estado_actual']

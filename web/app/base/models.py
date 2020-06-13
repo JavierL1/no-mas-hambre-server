83a9a7c507b50288f958307ae9f39db2adb63c35
Javier Levio <javier.levio.12@sansano.usm.cl>
2020-06-13 15:19:56 -0400

    Added model modifications

--- web/app/base/models.py
+++ web/app/base/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Persona(models.Model):
+    nombre = models.CharField('nombre', max_length=64)
+    apellido = models.CharField('apellido', max_length=64)
+
+
 class TipoContacto(models.Model):
     class Meta:
-        verbose_name_plural = "tipos contacto"
+        verbose_name_plural = 'tipos contacto'
 
     nombre = models.CharField('nombre', max_length=64)
 
@@ -13,29 +18,24 @@ class TipoContacto(models.Model):
 class Contacto(models.Model):
     tipo_contacto = models.ForeignKey(
         TipoContacto,
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='contactos'
     )
     contenido = models.CharField('url/usuario/numero', max_length=300)
-
-
-class Persona(models.Model):
-    nombre = models.CharField('nombre', max_length=64)
-    apellido = models.CharField('apellido', max_length=64)
-    contacto = models.OneToOneField(
-        Contacto,
-        models.SET_NULL,
+    persona = models.ForeignKey(
+        Persona,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='persona'
+        related_name='contactos'
     )
 
 
 class Region(models.Model):
     class Meta:
-        verbose_name_plural = "regiones"
+        verbose_name_plural = 'regiones'
 
     nombre = models.CharField('nombre', max_length=64)
     codigo = models.IntegerField('código')
@@ -48,7 +48,7 @@ class Comuna(models.Model):
     codigo = models.IntegerField('código')
     region = models.ForeignKey(
         Region,
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='comunas'
@@ -57,14 +57,19 @@ class Comuna(models.Model):
 
 class Lugar(models.Model):
     class Meta:
-        verbose_name_plural = "lugares"
+        verbose_name_plural = 'lugares'
 
     localidad = models.CharField('localidad', max_length=300)
     comuna = models.ForeignKey(
         Comuna,
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='lugares'
     )
-    direccion = models.CharField('direccion', max_length=300)
+    direccion = models.CharField(
+        'direccion',
+        max_length=300,
+        blank=True,
+        null=True
+    )
--- web/app/formularios/models.py
+++ web/app/formularios/models.py
@@ -6,28 +6,72 @@ from base.models import Lugar, Persona
 
 class Necesidad(models.Model):
     class Meta:
-        verbose_name_plural = "necesidades"
+        verbose_name_plural = 'necesidades'
 
-    dt_insercion = models.DateTimeField('fecha insercion')
+        def __str__(self):
+            return self.descripcion
+
+    fecha_insercion = models.DateTimeField('fecha insercion')
     remitente = models.ForeignKey(
         Persona,
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='necesidades_ingresadas'
     )
     ayudado = models.ForeignKey(
         Persona,
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='necesidades'
     )
     lugar = models.ForeignKey(
         Lugar,
-        models.SET_NULL,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
         related_name='necesidades_ubicadas'
     )
     descripcion = models.CharField('descripción', max_length=300)
+
+
+class EstadoAtencionNecesidad(models.Model):
+    class Meta:
+        verbose_name_plural = 'estados atencion necesidad'
+
+
+
+class AtencionNecesidad(models.Model):
+    class Meta:
+        verbose_name_plural = 'atenciones necesidad'
+
+    class Estados(models.TextChoices):
+        NO_ASIGNADA = 'NA'
+        ASIGNADA = 'A'
+        FINALIZADA = 'F'
+
+    necesidad = models.ForeignKey(
+        Necesidad,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='atenciones'
+    )
+    fecha_ingreso = models.DateTimeField('fecha ingreso')
+    fecha_objetivo = models.DateTimeField('fecha objetivo')
+    fecha_asignacion = models.DateTimeField(
+        'fecha asignacion',
+        blank=True,
+        null=True,
+    )
+    fecha_finalizacion = models.DateTimeField(
+        'fecha finalizacion',
+        blank=True,
+        null=True,
+    )
+    estado_actual = models.CharField(
+        max_length=2,
+        choices=Estados.choices,
+        default=Estados.NO_ASIGNADA,
+    )

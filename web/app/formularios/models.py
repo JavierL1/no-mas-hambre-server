c7afbb406ebd9cab8cd5239aecfdcd591876d334
Javier Levio <javier.levio.12@sansano.usm.cl>
2020-06-21 17:05:16 -0400

    Added Proveedor model. Refactored formularios migrations

--- web/app/formularios/models.py
+++ web/app/formularios/models.py
@@ -35,15 +35,20 @@ class Necesidad(models.Model):
     )
     lugar = models.ForeignKey(
         Lugar,
+        verbose_name='lugar',
         on_delete=models.SET_NULL,
         blank=False,
         null=True,
         related_name='necesidades'
     )
-    periodicidad = models.CharField('descripción', max_length=300)
-    descripcion = models.CharField('descripción', max_length=300)
+    periodicidad = models.CharField(
+        verbose_name='periodicidad', max_length=300
+    )
+    descripcion = models.CharField(verbose_name='descripción', max_length=300)
     fecha_finalizacion = models.DateTimeField(
-        verbose_name='fecha de finalización'
+        verbose_name='fecha de finalización',
+        blank=True,
+        null=True
     )
     tipo_gestion = models.ForeignKey(
         TipoGestion,
@@ -59,3 +64,31 @@ class Necesidad(models.Model):
         choices=EstadoNecesidad.choices,
         default=EstadoNecesidad.PENDIENTE
     )
+
+
+class Proveedor(models.Model):
+    tipos_gestion = models.ManyToManyField(
+        TipoGestion,
+        verbose_name='tipos de gestión',
+        related_name='proveedores'
+    )
+    despacho = models.BooleanField(verbose_name='despacho')
+    remitente = models.ForeignKey(
+        Participante,
+        verbose_name='remitente',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='proveedores'
+    )
+    lugar = models.ForeignKey(
+        Lugar,
+        verbose_name='lugar',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='proveedores'
+    )
+    descripcion = models.CharField(verbose_name='descripción', max_length=300)
+    rango = models.FloatField(verbose_name='rango')
+    activo = models.BooleanField(verbose_name='activo')

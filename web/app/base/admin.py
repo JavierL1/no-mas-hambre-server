49fd35187e0ed37344102621113bb3e91328eca3
Javier Levio <javier.levio.12@sansano.usm.cl>
2020-06-20 22:49:05 -0400

    Added new models to admin

--- web/app/base/admin.py
+++ web/app/base/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Comuna, Contacto, Lugar, Persona, Region
+from .models import Comuna, Contacto, Lugar, Organizacion, Participante
+from .models import Persona, Region, TipoGestion
 
 # Register your models here.
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Comuna)
 class ComunaAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Contacto)
-class ContactoAdmin(admin.ModelAdmin):
     pass
 
 
@@ -24,6 +24,21 @@ class PersonaAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Region)
-class RegionAdmin(admin.ModelAdmin):
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Organizacion)
+class OrganizacionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Participante)
+class ParticipanteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TipoGestion)
+class TipoGestionAdmin(admin.ModelAdmin):
     pass

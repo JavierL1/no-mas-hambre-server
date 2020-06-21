c6acf718a04019c5e64cd62515eaea353f4dc59a
Javier Levio <javier.levio.12@sansano.usm.cl>
2020-06-21 17:46:18 -0400

    Added Voluntario, Ayuda, Accion and Evento models

--- web/app/formularios/admin.py
+++ web/app/formularios/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
-from .models import Necesidad
+from .models import Accion, Ayuda, Evento, Necesidad, Proveedor, Voluntario
 
 # Register your models here.
 
 
+@admin.register(Accion)
+class AccionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ayuda)
+class AyudaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(Necesidad)
 class NecesidadAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Voluntario)
+class VoluntarioAdmin(admin.ModelAdmin):
     pass
--- web/app/formularios/models.py
+++ web/app/formularios/models.py
@@ -67,6 +67,7 @@ class Necesidad(models.Model):
 
 
 class Proveedor(models.Model):
+    fecha_insercion = models.DateTimeField(verbose_name='fecha de inserción')
     tipos_gestion = models.ManyToManyField(
         TipoGestion,
         verbose_name='tipos de gestión',
@@ -92,3 +93,105 @@ class Proveedor(models.Model):
     descripcion = models.CharField(verbose_name='descripción', max_length=300)
     rango = models.FloatField(verbose_name='rango')
     activo = models.BooleanField(verbose_name='activo')
+
+
+class Voluntario(models.Model):
+    fecha_insercion = models.DateTimeField(verbose_name='fecha de inserción')
+    remitente = models.ForeignKey(
+        Participante,
+        verbose_name='remitente',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='voluntarios'
+    )
+    descripcion = models.CharField(verbose_name='descripción', max_length=300)
+
+
+class Ayuda(models.Model):
+    nombre = models.CharField('nombre', max_length=64)
+    fecha_insercion = models.DateTimeField(verbose_name='fecha de inserción')
+    remitente = models.ForeignKey(
+        Participante,
+        verbose_name='remitente',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='ayudas'
+    )
+    tipo_gestion = models.ForeignKey(
+        TipoGestion,
+        verbose_name='tipo de gestión',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='ayudas'
+    )
+    lugar = models.ForeignKey(
+        Lugar,
+        verbose_name='lugar',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='ayudas'
+    )
+    descripcion = models.CharField(verbose_name='descripción', max_length=300)
+    rango = models.FloatField(verbose_name='rango')
+    activo = models.BooleanField(verbose_name='activo')
+
+
+class Accion(models.Model):
+    nombre = models.CharField('nombre', max_length=64)
+
+
+class Evento(models.Model):
+    necesidad = models.ForeignKey(
+        Necesidad,
+        verbose_name='necesidad',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='eventos'
+    )
+    proveedor = models.ForeignKey(
+        Proveedor,
+        verbose_name='proveedor',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='eventos'
+    )
+    voluntario = models.ForeignKey(
+        Voluntario,
+        verbose_name='voluntario',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='eventos'
+    )
+    ayuda = models.ForeignKey(
+        Ayuda,
+        verbose_name='ayuda',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='eventos'
+    )
+    lugar = models.ForeignKey(
+        Lugar,
+        verbose_name='lugar',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='eventos'
+    )
+    fecha = models.DateTimeField(verbose_name='fecha')
+    descripcion = models.CharField(verbose_name='descripción', max_length=300)
+    accion = models.ForeignKey(
+        Accion,
+        verbose_name='acción',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name='eventos'
+    )
